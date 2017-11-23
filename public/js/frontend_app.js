aadd8740638ed267cacc7109e9d337d9eb679f2b
philipauu <Phil@Phils-MacBook.local>
2017-11-23 11:39:02 +0800

    Forgetfulness, n.: 	A gift of God bestowed upon debtors in compensation for 	their destitution of conscience.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,1 +1,71 @@
 console.log('loaded frontend app');
+
+var frontend_app = angular.module('employees', []);
+
+frontend_app.controller('emp_data', do_data);
+
+function do_data($scope, $http) {
+    console.log('inside do_data');
+
+    $scope.read = function () {
+        console.log('reading all records');
+
+        $http.get('/api/v4/read')
+            .then(function (results) {
+                console.log(results);
+                $scope.employees = results.data;
+            });
+    }
+
+    $scope.read();
+
+    $scope.create = function () {
+        console.log('creating new employee');
+
+        var data = {
+            name: $scope.input.name,
+            gender: $scope.input.gender,
+            email: $scope.input.email,
+            avatar: $scope.input.avatar
+        };
+
+        $http.post('/api/v4/create', data)
+            .then(function (result) {
+                console.log(result);
+                $scope.message = result.data.message;
+                $scope.read();
+            });
+    }
+
+    $scope.update = function (employee) {
+        console.log('updating employee');
+        console.log(employee);
+
+        // var data = {
+        //     _id: employee._id,
+        //     name: employee.name,
+        //     gender: employee.gender,
+        //     email: employee.email,
+        //     avatar: employee.avatar
+        // };
+
+        $http.put('/api/v4/update', employee)
+            .then(function (result) {
+                console.log(result);
+                $scope.message = result.data.message;
+                $scope.read();
+            });
+    }
+
+    $scope.delete = function (employee) {
+        console.log('deleting employee');
+        console.log(employee);
+        $http.delete('/api/v2/delete' + employee._id)
+            .then(function (result) {
+                console.log(result);
+                $scope.message = result.data.message;
+                $scope.read();
+            });
+    }
+
+}
